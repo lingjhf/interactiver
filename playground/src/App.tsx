6263f23e63b5d16497395fe7de123dab2531090d
fengjianhao <lingj.jhf@outlook.com>
2024-03-29 10:37:04 +0800

    dev

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -4,6 +4,13 @@ import * as d3 from 'd3'
 
 import { generateTree } from './utils'
 
+function createShadow() {
+  const filter = d3.create('svg:filter').attr('id', 'drop-shadow').attr('x', '-20%').attr('y', '-20%').attr('width', 100).attr('height', 70)
+  const shadow = d3.create('svg:feDropShadow').attr('dx', 4).attr('dy', 4).attr('stdDeviation', 4).attr('flood-color', '#000000').attr('flood-opacity', 0.4)
+  filter.append(() => shadow.node())
+  return filter
+}
+
 function App() {
   let interactiveView: InteractiveView
 
@@ -15,6 +22,7 @@ function App() {
         background.setAttribute('width', `${container.width}px`)
         background.setAttribute('height', `${container.height}px`)
         background.setAttribute('fill', '#fff')
+        background.setAttribute('filter', 'url(#drop-shadow)')
         container.element.appendChild(background)
         const text = createElement('text')
         text.innerHTML = node.id
@@ -57,9 +65,12 @@ function App() {
   const res = generateTree(tree.layout, 5)
   tree.layout.add(res[0])
   function initInteractiveView(el: HTMLDivElement) {
+    const shadow = createShadow()
+
     interactiveView = new InteractiveView(el)
     interactiveView.zoom.setAll(0.1)
     interactiveView.element.innerHTML = ''
+    interactiveView.element.appendChild(shadow.node()!)
     interactiveView.element.append(...tree.autoLayout({ rankdir: 'LR', nodesep: 100, ranksep: 1000 }).elements)
   }
 
